de0e9638b11f01ff5c68378979bda9b938bf71e3
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-04-01 18:46:02 +0300

    clean up controller

--- XafMT2.WebApi/API/SyncController.cs
+++ XafMT2.WebApi/API/SyncController.cs
@@ -26,55 +26,14 @@ namespace XafMT2.WebApi.API
 
         public async override Task<string> Push()
         {
+            string NodeId = GetHeader("NodeId");
 
-
-            PushOperationResponse pushOperationResponse = new PushOperationResponse();
-            try
-            {
-                string NodeId = GetHeader("NodeId");
-                StreamReader streamReader = new StreamReader(base.Request.Body);
-                string s = await streamReader.ReadToEndAsync();
-                using MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(s));
-                DataContractJsonSerializer dataContractJsonSerializer = new DataContractJsonSerializer(typeof(List<Delta>));
-                List<Delta> Deltas = (List<Delta>)dataContractJsonSerializer.ReadObject((Stream)ms);
-                await _SyncServer.SaveDeltasAsync(NodeId, Deltas, default(CancellationToken));
-                string message = $"Push to node:{NodeId}{Environment.NewLine}Deltas Received:{Deltas.Count}{Environment.NewLine}Identity:{Deltas.FirstOrDefault()?.Identity}";
-                _logger.LogInformation(message);
-                pushOperationResponse.Success = true;
-                pushOperationResponse.Message = message;
-            }
-            catch (NodeNotFoundException ex)
-            {
-                _logger.LogError(ex, "An argument null exception occurred.");
-                pushOperationResponse.Success = false;
-                pushOperationResponse.Message = ex.Message;
-            }
-            catch (ArgumentNullException exception)
-            {
-                _logger.LogError(exception, "An argument null exception occurred.");
-            }
-            catch (InvalidOperationException exception2)
-            {
-                _logger.LogError(exception2, "An invalid operation exception occurred.");
-            }
-            catch (Exception exception3)
-            {
-                _logger.LogError(exception3, "An unknown exception occurred.");
-            }
-
-            DataContractJsonSerializer dataContractJsonSerializer2 = new DataContractJsonSerializer(typeof(PushOperationResponse));
-            MemoryStream memoryStream = new MemoryStream();
-            dataContractJsonSerializer2.WriteObject((Stream)memoryStream, (object?)pushOperationResponse);
-            memoryStream.Position = 0L;
-            StreamReader streamReader2 = new StreamReader(memoryStream);
-            return streamReader2.ReadToEnd();
-
-            //  return base.Push();
+            return await base.Push();
         }
 
-        public override Task<IActionResult> RegisterNode()
+        public override async Task<IActionResult> RegisterNode()
         {
-            return base.RegisterNode();
+            return await base.RegisterNode();
         }
     }
 
